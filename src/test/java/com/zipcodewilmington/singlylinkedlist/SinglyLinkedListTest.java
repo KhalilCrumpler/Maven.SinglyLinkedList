c70b0831db536cef84cd36bce7685add425461d6
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-09 17:47:41 -0400

    added more tests

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -37,4 +37,18 @@ public class SinglyLinkedListTest {
         Assert.assertEquals(-1, list.indexOf(50));
     }
 
+    @Test
+    public void testContains(){
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        //then
+        Assert.assertTrue(list.contains(20));
+        Assert.assertFalse(list.contains(70));
+    }
+
 }
